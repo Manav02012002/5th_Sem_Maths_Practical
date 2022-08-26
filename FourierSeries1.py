ad88f0938e86248f3e8f7fc09e6836dd957ba4c7
Manav Madan Rawal <80583498+Manav02012002@users.noreply.github.com>
2022-08-27 01:20:22 +0530

    Add files via upload

---
+++ FourierSeries1.py
@@ -0,0 +1,9 @@
+# MT5P1 Experiment 1 
+# Program to calculate Fourier Series
+# Manav Madan Rawal 20PCM53
+
+from sympy import*
+x = Symbol('x')
+s = fourier_series(x**2,(x,-pi,pi))
+k = s.truncate(n=4)
+print("The Fourier Series Expansion of ", x**2, "is f(x) = ",k," ...")
