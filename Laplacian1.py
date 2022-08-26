b2937b192d58f009e4d42018bb4403877ada2fa8
Manav Madan Rawal <80583498+Manav02012002@users.noreply.github.com>
2022-08-27 01:06:51 +0530

    Add files via upload

---
+++ Laplacian1.py
@@ -0,0 +1,13 @@
+# MT5P2 Experiment 1 
+# Program to calculate the Curl
+# Manav Madan Rawal 20PCM53
+
+from sympy import*
+x,y,z = symbols("x,y,z")
+def laplacian(f):
+    a = diff(f,x,2)
+    b = diff(f,y,2)
+    c = diff(f,z,2)
+    print("Laplacian is ",a+b+c)
+f = 3*(x**2) + 2*(y**3) - 5*z
+laplacian(f)
